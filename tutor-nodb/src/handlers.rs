aed79b3117d5cd698d3416d555bf33094007e2f1
lwooden <lowell.wooden@gmail.com>
2021-12-25 12:47:23 -0500

    added test suite

--- tutor-nodb/src/handlers.rs
+++ tutor-nodb/src/handlers.rs
@@ -57,7 +57,7 @@ pub async fn get_all_courses_for_tutor(app_state: web::Data<AppState>, params: w
         .unwrap() // subsequently we must unwrap
         .clone()
         .into_iter() // convert courses into an iterator 
-        .filter(|course| course.tutor_id == tutor_id)
+        .filter(|course| course.tutor_id == tutor_id) // a closure (or anonymous function) with 1 parameter
         .collect::<Vec<Course>>(); // turn filtered result into a collection
 
         if filtered_courses.len() > 0 {
@@ -65,6 +65,71 @@ pub async fn get_all_courses_for_tutor(app_state: web::Data<AppState>, params: w
         } else {
             HttpResponse::Ok().json("No courses found for tutor".to_string())
         }
-
 }
 
+pub async fn get_course_detail(app_state: web::Data<AppState>, params: web::Path<(usize,usize)>) -> HttpResponse {
+
+    let (tutor_id, course_id) = params.0;
+
+    let selected_courses = app_state
+      .courses
+      .lock()
+      .unwrap()
+      .clone()
+      .into_iter()
+      .find(|x| x.tutor_id == tutor_id && x.course_id == Some(course_id)) // Some is used here because course_id is of type Option
+      .ok_or("Course not found!");
+
+    // The `if let` construct reads: "if `let` destructures `selected_course` into
+    // `Ok(course)`, evaluate the block (`{}`).
+      if let Ok(course) = selected_courses {
+          HttpResponse::Ok().json(course)
+      } else {
+          HttpResponse::Ok().json("Course not found".to_string())
+      }
+}
+
+
+// Test Suite
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::StatusCode;
+    use std::sync::Mutex;
+
+    #[actix_rt::test]
+    async fn post_course_test() {
+        let course  = web::Json(Course {
+            tutor_id: 1,
+            course_name: "Hello, this is test course".into(),
+            course_id: None,
+            posted_time: None,
+        });
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            courses: Mutex::new(vec![]),
+        });
+        let resp = new_course(app_state, course).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn get_all_courses_for_tutor_test() {
+
+        // Initialize AppState
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            courses: Mutex::new(vec![]),
+        });
+
+        // Simulate parameters based on the Course object we created above
+        let tutor_id: web::Path<usize> = web::Path::from(1);
+
+        // Call handler and supply the newly updated app_state along with params for the request
+        let resp = get_all_courses_for_tutor(app_state, tutor_id).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+}
